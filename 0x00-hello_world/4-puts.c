bc15f88d050fdbca60398e79c0009c299af902f3
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 13:25:45 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,6 @@
-echo "$(puts "Programming is like building a multilingual puzzle")"
+#include <stdio.h>
+
+int main(void) {
+	    puts("Programming is like building a multilingual puzzle");
+	        return 0;
+
