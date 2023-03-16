0a4d7b5363d7051e214d2232dc4c754a7e82abf2
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 12:48:16 -0700

    ff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,1 @@
-/**
- * main - Entry point for the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	    puts("Programming is like building a multilingual puzzle");
-	        return (0);
-}
+echo "$(puts "Programming is like building a multilingual puzzle")"
