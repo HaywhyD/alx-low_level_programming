5d16ed8b270e604fd845bb82b2600bcb940a95c3
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 13:35:34 -0700

    ff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-	    puts("Programming is like building a multilingual puzzle");
-	        return 0;
+/**
+* main - Entry point for the program
+*
+* Return: Always 0 (Success)
+*/
 
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
