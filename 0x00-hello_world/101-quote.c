3b48a50e97a7454899fe2611fbbd529ae2ef083e
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 19:25:00 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
+*
 * Return: Always 1 (failure)
 */
 int main(void)
 {
-fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
