f9a791cbd937e31ccf0950a8a485924972643c20
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 18:08:29 -0700

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+const char msg[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, msg, sizeof(msg) - 1);
 return (1);
 }
