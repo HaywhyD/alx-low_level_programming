91c80f949612ede4213a9457b3433f24f20e5729
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 17:09:39 -0700

    f

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the program
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf(sizeof(char));
+	printf(sizeof(short));
+	printf(sizeof(int));
+	printf(sizeof(long));
+	printf(sizeof(long long));
+	printf(sizeof(float));
+	printf(sizeof(double));
+	printf(sizeof(long double));
+	return (0);
+}
