458784b0fdbf0c7a9604155b9d27b923246cfc49
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-16 17:15:39 -0700

    ff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	printf(sizeof(char));
-	printf(sizeof(short));
-	printf(sizeof(int));
-	printf(sizeof(long));
-	printf(sizeof(long long));
-	printf(sizeof(float));
-	printf(sizeof(double));
-	printf(sizeof(long double));
-	return (0);
+printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of short: %ld byte(s)\n", sizeof(short));
+printf("Size of int: %ld byte(s)\n", sizeof(int));
+printf("Size of long: %ld byte(s)\n", sizeof(long));
+printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+printf("Size of float: %ld byte(s)\n", sizeof(float));
+printf("Size of double: %ld byte(s)\n", sizeof(double));
+printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+return (0);
 }
