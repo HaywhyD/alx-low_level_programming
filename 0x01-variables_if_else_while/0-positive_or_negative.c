086a9ec453ab512fd8a551192614354d9307d822
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-17 18:39:52 -0700

    ddd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("$n: is positive");
+printf("%d is positive\n", n);
 }
 if (n == 0)
 {
-printf("$n: is zero\n");
+printf("%d is zero\n", n);
 }
 if (n < 0)
 {
-printf("$n: is negative\n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
