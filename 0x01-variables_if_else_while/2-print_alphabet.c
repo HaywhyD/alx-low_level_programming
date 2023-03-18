7c2675cc022d5d547d43b96f41aa5fd97a061cac
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-17 19:36:17 -0700

    dd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
