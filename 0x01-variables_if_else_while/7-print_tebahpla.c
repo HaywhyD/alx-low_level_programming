02049369aeca3cccd45aa8b0e9aaa8b1be2fc84a
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-17 20:44:43 -0700

    jdu

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * * main - Entry point of the program
+ * *
+ * * Return: Always 0
+ * */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
