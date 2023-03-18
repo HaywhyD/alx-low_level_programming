3dd6cfb89f32102369cbc23509f81f1f14271424
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-17 21:28:22 -0700

    jof

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int  c;
+for (c = 0; c < 10; c++)
+{
+putchar(c);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
