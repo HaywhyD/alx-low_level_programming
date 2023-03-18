3ac4861cc8b9ccbc8aee7136ac8657a8c296f5e6
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-17 19:22:00 -0700

    ff

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,19 +13,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int last = n % 10;
 printf("Last digit of %d is ", n);
-if (last > 5)
+if ((n % 10) > 5)
 {
-printf("%d is greater than 5\n", last);
+printf("%d is greater than 5\n", (n % 10));
 }
-else if (last == 0)
+else if ((n % 10) == 0)
 {
-printf("%d is zero\n", last);
+printf("%d is zero\n", (n % 10));
 }
 else
 {
-printf("%d and is less than 6 and not 0\n", last);
+printf("%d and is less than 6 and not 0\n", (n % 10));
 }
 return (0);
 }
