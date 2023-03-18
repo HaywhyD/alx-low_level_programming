ab24f9a5728013a4379ea4c84eee80768e478244
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-17 20:00:43 -0700

    fd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+printf("%d", c);
+}
+}
+putchar('\n')
+return (0);
+}
