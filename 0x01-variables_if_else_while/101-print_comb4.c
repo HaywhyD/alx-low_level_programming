3a0287d0932c20dfc9fe2987b1c385dd0110ddb8
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-18 23:10:26 -0700

    d

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,14 +6,14 @@
 *
 * Return: Always 0
 */
-int main()
+int main(void)
 {
 int i, j, k;
 for (i = 0; i < 8; i++)
 {
 for (j = i + 1; j < 9; j++)
 {
-for(k = j + 1; k < 10; k++)
+for (k = j + 1; k < 10; k++)
 {
 putchar('0' + i);
 putchar('0' + j);
@@ -24,7 +24,7 @@ putchar(',');
 putchar(' ');
 }
 }
-}    
+}
 }
 putchar('\n');
 return (0);
