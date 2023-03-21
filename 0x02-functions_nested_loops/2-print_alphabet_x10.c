bca49c234d9658529d4a4db771d7e31a6b51ea4e
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-21 16:29:43 -0700

    ddd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_alphabet - prints the lowercase alphabet followed by a new line
+* print_alphabet_x10 - prints the lowercase alphabet followed by a new line
 */
 void print_alphabet_x10(void)
 {
