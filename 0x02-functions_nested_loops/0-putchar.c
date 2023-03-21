e1958ec491011a62aec40a7f53dbb60856db9874
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-21 15:03:52 -0700

    nd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
