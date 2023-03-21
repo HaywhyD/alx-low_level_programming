a91a5db390de28ea2b4bc0efe741fd32786d0282
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-21 15:38:43 -0700

    dd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+/**
+* print_alphabet - Prints letters of the aphabet
+*/
+void print_alphabet(void)
+{
+char c;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n);
+}
