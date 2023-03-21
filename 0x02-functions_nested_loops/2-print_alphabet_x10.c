a7d70455d96871c13d141177afb4b9197db4db96
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-21 16:24:37 -0700

    d

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - prints the lowercase alphabet followed by a new line
+*/
+void print_alphabet_x10(void)
+{
+char c;
+for (int i = 0; i <10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
