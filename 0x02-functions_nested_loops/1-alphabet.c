a172ce349ef329fb1de4cbbf3bb202920b877594
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-21 16:04:12 -0700

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <main.h>
-/**
-* main - Entry point of the program
-*
-* Return: Always 0
-*/
-int main(void)
-{
-	printf("_putchar\n");
-	return (0);
-}
 
 /**
 * print_alphabet - Prints letters of the aphabet
@@ -20,7 +10,7 @@ void print_alphabet(void)
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(i);
+_putchar(c);
 }
 _putchar('\n');
 }
