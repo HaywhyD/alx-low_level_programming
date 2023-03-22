8616e4312d941dbbe820b47975ea30d4b528197c
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-21 18:52:25 -0700

    dd

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* _islower -prints lowercase characters
+*
+* Return: Always 0 or 1
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
