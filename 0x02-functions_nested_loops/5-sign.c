0f60bfde57f0104a61b1e2037d64bc7f0cd36550
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-22 11:01:00 -0700

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - prints lowercase characters
+*
+* @n: integer parameter
+*
+* Return: Always 0 or 1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}
