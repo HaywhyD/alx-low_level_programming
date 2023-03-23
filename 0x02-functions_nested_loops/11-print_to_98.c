bb1f0118ada54171eaa3606f7062b5c6954aac9f
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-23 09:24:57 -0700

    d

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,88 @@
 */
 void print_to_98(int n)
 {
-for (n = n; n <= 98; n++)
+int i;
+if (n <= 98 && n >= 0)
 {
-if (n < 10)
+for (i = n; i <= 98; i++)
 {
-_putchar(n % 10 + '0');
+if (i < 10)
+{
+_putchar(i % 10 + '0');
 }
 else
 {
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 }
-if (n != 98)
+if (i != 98)
 {
 _putchar(',');
 _putchar(' ');
 }
 }
 _putchar('\n');
+}
+else if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+if (i >= 100)
+{
+_putchar(i / 100 + '0');
+}
+_putchar(i / 10 % 10 + '0');
+_putchar(i % 10 + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+else
+{
+int abs = _abs64(n);
+for (i = abs; i >= 0; i--)
+{
+if (i != 0)
+{
+_putchar('-');
+}
+if (i < 10)
+{
+_putchar(i % 10 + '0');
+}
+else
+{
+if (i >= 100)
+{
+_putchar(i / 100 + '0');
+}
+_putchar(i / 10 % 10 + '0');
+_putchar(i % 10 + '0');
+}
+_putchar(',');
+_putchar(' ');
+}
+for (i = 1; i <= 98; i++)
+{
+if (i < 10)
+{
+_putchar(i % 10 + '0');
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
