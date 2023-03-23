9f5da5b2f56e35b51f9bd619570db43eaf26ac9e
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-23 04:57:20 -0700

    dd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit - prints lowercase characters
+*
+* @num: integer parameter
+*
+* Return: Always 0 or 1
+*/
+int print_last_digit(int num)
+{
+int last_digit = num % 10;
+return (last_digit);
+}
