b907e1fb186f86ade0dc83eabb829e7e2ea6aefc
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-23 06:56:54 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,6 +17,15 @@ if (product < 10)
 _putchar(' ');
 _putchar(product + '0');
 }
+else if (j == 0)
+{
+_putchar(product % 10 + '0');
+}
+else if (i == 0)
+{
+_putchar(' ');
+_putchar(product + '0');
+}
 else
 {
 _putchar(product / 10 + '0');
