da4d305a0d8e96e76237d0e48f9c71f3fa05a8af
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-23 07:15:32 -0700

    ss

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* add - prints lowercase characters
+*
+* @int: integer parameter
+* Return: Always 0 or 1
+*/
+void add(int, int)
+{
+int i, j;
+int sum = i + j;
+return (sum);
+}
