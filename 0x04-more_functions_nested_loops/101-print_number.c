85889d60b4c56d9fc7c1ff0d7c8796446c7f2321
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-24 19:05:16 -0700

    dd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,31 @@ void print_number(int n)
 {
 if (n < 0)
 {
-putchar('-');
-n = -n;
-}
-if (n / 10)
-{
-print_number(n / 10);
-}
+n = -1 * n;
+_putchar('-');
+_putchar(n / 10 + '0');
 _putchar((n % 10) + '0');
+}
+else if (n < 100 && n >= 10)
+{
+_putchar(n / 10 + '0');
+_putchar((n % 10) + '0');
+}
+else if (n < 10 && n > 0)
+{
+_putchar((n % 10) + '0');
+}
+else if (n > 999)
+{
+_putchar(n / 1000 + '0');
+_putchar(n % 1000 / 100 + '0');
+_putchar(n % 100 / 10 + '0');
+_putchar((n % 10) + '0');
+}
+else if (n > 100 && n < 1000)
+{
+_putchar(n / 100 + '0');
+_putchar(n % 100 / 10 + '0');
+_putchar((n % 10) + '0');
+}
 }
