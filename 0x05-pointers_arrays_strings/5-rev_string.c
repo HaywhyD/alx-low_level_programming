a3d6f25f42f23fd4c51820bc3135e98c9a89d918
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-27 16:56:01 -0700

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 #include <stdio.h>
 /**
 * rev_string - Prints a string in reverse, followed by a new line.
@@ -7,16 +8,13 @@
 */
 void rev_string(char *s)
 {
-int len = 0;
-while (*s != '\0')
+int len = strlen(s);
+int i, j;
+char temp;
+for (i = 0, j = len - 1; i < j; i++, j--)
 {
-len++;
-s++;
-}
-s--;
-while (len > 0)
-{
-s--;
-len--;
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
 }
 }
