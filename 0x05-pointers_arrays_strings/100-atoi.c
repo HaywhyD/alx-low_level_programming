320a41df214408ecb8c7da36d529f5d261f89361
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-27 18:17:39 -0700

    s

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* _atoi - Converts a string to an integer.
+* @s: The string to be converted.
+*
+* Return: The converted integer.
+*/
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+{
+sign = -sign;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+}
+else if (result > 0)
+{
+/* We've reached the end of the number */
+break;
+}
+i++;
+}
+return (sign * result);
+}
