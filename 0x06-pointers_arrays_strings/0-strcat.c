17eecd6732799981804ac390f156ea54fc3b4884
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-29 18:20:08 -0700

    d

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#imclude "main.h"
+/**
+* _strcat - Concatenates two strings.
+* @dest: Pointer to the destination string.
+* @src: Pointer to the source string.
+*
+* Return: Pointer to the resulting string dest.
+*/
+char *_strcat(char *dest, const char *src)
+{
+size_t dest_len = 0;
+size_t i;
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[dest_len + i] = src[i];
+}
+dest[dest_len + i] = '\0';
+return (dest);
+}
