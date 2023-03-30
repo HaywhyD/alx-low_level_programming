a784b8935c50ad29da180ea22b160f44430e1ed3
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-29 19:58:59 -0700

    d

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-int _putchar(char *);
+int _putchar(char c);
 void print_number(int n);
 char *rot13(char *);
