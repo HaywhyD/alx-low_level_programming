78dbeac6c338e067fe676c68ecab91e230f3a2af
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-03-30 18:13:10 -0700

    d

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,15 +10,9 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i = 0, j = 0, k = 0, carry = 0, sum = 0;
-while (n1[i] != '\0')
-{
-i++;
-}
-while (n2[j] != '\0')
-{
-j++;
-}
+int i, j, k, carry = 0, sum = 0;
+for (i = 0; n1[i] != '\0'; i++);
+for (j = 0; n2[j] != '\0'; j++);
 if (i > size_r || j > size_r)
 {
 return (0);
@@ -27,7 +21,7 @@ r[size_r] = '\0';
 i--;
 j--;
 size_r--;
-while (i >= 0 || j >= 0)
+for (; i >= 0 || j >= 0; i--, j--, size_r--)
 {
 sum = carry;
 if (i >= 0)
