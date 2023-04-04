073e38785204b5c42d73c6d835a3c1a164294d27
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-04-03 17:22:48 -0700

    d

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,9 +12,8 @@ void print_diagsums(int *a, int size)
 int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
-sum1 += *(a + i * size + i);
-sum2 += *(a + i * size + size - 1 - i);
+sum1 += *(a + i*size + i);
+sum2 += *(a + i*size + (size-i-1));
 }
-printf("Sum of diagonal1 is %d\n", sum1);
-printf("Sum of diagonal2 is %d\n", sum2);
+printf("%d, %d\n", sum1, sum2);
 }
