0f93e87f3634aa1f8435a939c230a1cff491b50b
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-04-18 00:34:27 -0700

    he

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the name of file compiled from
+* main - prints the name of file compiled from
+* Return: 0 or 1
 */
 int main(void)
 {
 printf("%s\n", __FILE__);
+return(0);
 }
