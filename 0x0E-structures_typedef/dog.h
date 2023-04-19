b7ca60bf82010a83330551d0d26b0e862e0d80e6
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-04-19 13:40:22 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,11 +19,5 @@ char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-typedef struct dog dog_t;
-int _strlen(char *s);
-char *strcpy(char *dest, char *src);
 
 #endif
