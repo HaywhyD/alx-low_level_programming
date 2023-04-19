a51b2af2b4ef46310f55eada428e488e50723e40
Oreoluwa Adejumo <adejumooreoluwa@gmail.com>
2023-04-19 13:21:25 -0700

    f

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
-/*
-* struct dog - new struct
+
+/**
+* struct dog - new struct for dog
+*
 * @name: name of dog
+*
 * @age: age of dog
+*
 * @owner: owner of dog
-*/ 
+**/
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
 #endif
