6a47247551467f46e4b0dddc88da487fd28d25da
Oreoluwa Adejumo <adejumooreoluwa@mail.com>
2023-10-25 13:48:56 -0700

    created 0x08-recursion folder and files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts_recursion - Prints a string followed by a new line.
+*
+* @str: The string to print.
+*/
+void _puts_recursion(char *s)
+{
+_putchar(s);
+_putchar("\n");
+}
