eb99cc695b38df9a6d841224f00daf22f08a1d61
Oreoluwa Adejumo <adejumooreoluwa@mail.com>
2023-10-25 15:53:41 -0700

    created int is_prime_number(int n) file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,17 +2,22 @@
 /**
 * is_prime_number - Checks if a number is prime.
 * @n: The integer to check.
+* @divisor: The current divisor being checked.
 *
 * Return: 1 if n is prime, 0 otherwise.
 */
-int is_prime_number(int n)
+int is_prime_helper(int n, int divisor)
 {
 if (n <= 1)
+	return 0;
+if (divisor * divisor > n)
+	return (1);
+if (n % divisor == 0)
 	return (0);
-for (int i = 2; i * i <= n; i++)
-{
-if (n % i == 0)
-	return (0);
+return (is_prime_helper(n, divisor + 1));
 }
-return (1);
+
+int is_prime_number(int n)
+{
+return (is_prime_helper(n, 2));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int find_sqrt(int n, int guess);
+int is_prime_helper(int n, int divisor);
 int _putchar(char c);
