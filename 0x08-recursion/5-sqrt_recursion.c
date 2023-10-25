5bff6a3eb7ba85b11096f5557faf899d92ec8758
Oreoluwa Adejumo <adejumooreoluwa@mail.com>
2023-10-25 15:19:10 -0700

    made changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-	return -1;
+	return (-1);
 else if (n == 0 || n == 1)
 	return (n);
 else
@@ -29,7 +29,7 @@ int find_sqrt(int n, int guess)
 if (guess * guess == n)
 	return (guess);
 else if (guess * guess > n)
-	return 	(-1);
+	return (-1);
 else
 	return (find_sqrt(n, guess + 1));
 }
