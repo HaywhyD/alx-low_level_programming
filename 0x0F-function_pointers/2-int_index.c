1cce0fa6a948c1dbfdbcd31aa30ec06141f80541
Oreoluwa Adejumo <adejumooreoluwa@mail.com>
2023-11-09 11:42:56 -0800

    completed the function pointers and varidic functions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * int_index - earches for an integer
+ * int_index - searches for an integer.
  * @array: array to search in
  * @size: size of the array
  * @cmp: pointer to the comparing function
- *
- * Return: index of the first element for which
- * the cmp function does not return 0, or -1 if no match is found
+ * Return: index of the first element for which the cmp
+ * function does not return 0, or -1 if no match is found
  * or size is negative
  */
 int int_index(int *array, int size, int (*cmp)(int))
@@ -22,6 +21,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 				return (i);
 		}
 	}
-
 	return (-1);
 }
